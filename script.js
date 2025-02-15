bb3e3e6bcee8faaea497606668e1bcdd653bb9a3
TopboySaint <topboysaint1@gmail.com>
2025-02-15 21:03:10 +0100

    Add initial implementation of E-Commerce cart functionality with HTML, CSS, and JavaScript

---
+++ script.js
@@ -0,0 +1,56 @@
+var itemsCart = [];
+// var myCartT
+
+function addme() {
+  if (floatingInput.value === "") {
+    document.getElementById("errorMsg").style.display = "block";
+  } else {
+    document.getElementById("errorMsg").style.display = "none";
+    itemsCart.push(floatingInput.value);
+    console.log(itemsCart);
+    floatingInput.value = ""; 
+    displayTable();
+  }
+}
+
+function displayTable() {
+  var myTable = "<table>";
+  myTable += "<tr>";
+  myTable += `<th class="serialNumber ps-4"> Serial Number(s) </th>`;
+  myTable += '<th class= "products ps-4">' + "Products" + "</th>";
+  myTable += "</tr>";
+
+  for (i = 0; i < itemsCart.length; i++) {
+    myTable += "<tr>";
+    myTable += '<td class="ps-5">' + `${i + 1}` + "</td>";
+    myTable += '<td class="ps-5">' + `<span> ${itemsCart[i]} </span>` + "</td>";
+    myTable += "</tr>";
+  }
+
+  myTable += "</table>";
+  document.getElementById("items").innerHTML = myTable;
+}
+
+function delFirst() {
+  itemsCart.shift();
+  displayTable();
+}
+
+function delLast() {
+  itemsCart.pop();
+  displayTable();
+}
+
+function editItem() {
+
+}
+
+function addStart(){
+    itemsCart.unshift(floatingInput.value)
+    displayTable()
+}
+
+function delAll(){
+    itemsCart.splice(itemsCart.length)
+    displayTable()
+}
